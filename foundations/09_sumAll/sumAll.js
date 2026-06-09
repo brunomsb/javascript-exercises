04b2336600d4ac04e342d973401851a591a48cf2
Bruno <bruno.msousab@gmail.com>
2026-06-09 12:00:37 +0100

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    if (a !== Math.floor(a) || b !== Math.floor(b)) return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
 
+    let lowerBound = a <= b ? a : b;
+    let upperBound = a <= b ? b : a;
+    
+    let sum = 0;
+    for (let i = lowerBound; i <= upperBound; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
