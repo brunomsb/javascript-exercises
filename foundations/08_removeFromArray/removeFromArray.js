f2d1608ef28727cbb5c0a212fd737fd31c882057
Bruno <bruno.msousab@gmail.com>
2026-06-09 11:49:26 +0100

    Finish 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
+    args.shift();
+    let result = array.filter((item) => !args.includes(item));
+    return result;
 };
 
 // Do not edit below this line
