2205c60576c927e3bed462709041dcf1be879008
Bruno <bruno.msousab@gmail.com>
2026-06-09 12:18:56 +0100

    Finish 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+/*
+ IF year IS divisible by 4 AND IS NOT divisible by 100 => TRUE
+ ELSEIF year IS divisible by 4 AND IS divisible by 100 AND IS divisible by 400 => TRUE
+ ELSE => FALSE
+ ENDIF
+*/
 
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByAHundred = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
+    
+    if (isDivisibleByFour && (!isDivisibleByAHundred || isDivisibleByFourHundred)) return true;
+    else return false;
 };
 
 // Do not edit below this line
