c02bf4a9f2ebf40f57e46ad7b2b3e039b10d2087
Bruno <bruno.msousab@gmail.com>
2026-06-29 11:55:37 +0100

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const strNoPunctuation = function (str) {
+    return str.replaceAll(" ", "").replaceAll(",", "").replaceAll(".", "").replaceAll("!", "").toLowerCase();
+}
 
+const getReversedStr = function (str) {
+    return str.split("").reverse().join("");
+}
+
+const palindromes = function (str) {
+    const noPunctuation = strNoPunctuation(str);
+    const reversed = getReversedStr(noPunctuation);
+    return reversed === noPunctuation;
 };
+
+const noPunctuation = strNoPunctuation("A car, a man, a maraca.");
+const reversed = getReversedStr(noPunctuation);
+console.log(noPunctuation);
+console.log(reversed);
 
 // Do not edit below this line
 module.exports = palindromes;
