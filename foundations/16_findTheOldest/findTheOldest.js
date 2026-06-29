4824dfee7fe735452b24e79593fe407175461beb
Bruno <bruno.msousab@gmail.com>
2026-06-29 12:34:31 +0100

    Finish 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function getAge(person) {
+    const date = new Date();
+    return person.yearOfDeath ?
+        person.yearOfDeath - person.yearOfBirth :
+        date.getFullYear() - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(current);
+        
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
