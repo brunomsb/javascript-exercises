6a36f15fb0c1070fa7f89b3d5b1d207d652f7ade
Bruno <bruno.msousab@gmail.com>
2026-06-29 12:10:44 +0100

    Finish 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = +a;
 
+    if (a === 0) return 0;
+    if (a < 0) return "OOPS";
+
+    let sequence = [1, 1];
+    function getNext(seq) {
+        return seq[seq.length-1] + seq[seq.length-2];
+    }
+
+    for (let i = 3; i <= a; i++) {
+        sequence.push(getNext(sequence));
+    }
+
+    return sequence[sequence.length-1];
 };
 
 // Do not edit below this line
